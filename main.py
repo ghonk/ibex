8474e687592e640f3ae3e9ddc0a2cdd672fa17c1
Ubuntu <azunre@PaulLinuxDev.jaavmej2qrwupdw4djg0r2u5ud.bx.internal.cloudapp.net>
2017-11-02 17:14:38 +0000

    basic NER in main.py, not yet a class, etc

--- main.py
+++ main.py
@@ -5,44 +5,12 @@ Created on Tue Oct 31 17:22:41 2017
 @author: azunre
 """
 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.base import TransformerMixin
-from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
-from sklearn.metrics import accuracy_score
 from nltk.corpus import stopwords
 import string
 import re
-
-# Some prelims =>
-
-# Set up spaCy
-from spacy.en import English
-parser = English()
-
-# A custom stoplist
-STOPLIST = set(stopwords.words('english') + ["n't", "'s", "'m", "ca"] + list(ENGLISH_STOP_WORDS))
-# List of symbols we don't care about
-SYMBOLS = " ".join(string.punctuation).split(" ") + ["-----", "---", "...", "“", "”", "'ve"]
-
-# Every step in a pipeline needs to be a "transformer". 
-# Define a custom transformer to clean text using spaCy
-class CleanTextTransformer(TransformerMixin):
-    """
-    Convert text to cleaned text
-    """
-
-    def transform(self, X, **transform_params):
-        return [cleanText(text) for text in X]
-
-    def fit(self, X, y=None, **fit_params):
-        return self
-
-    def get_params(self, deep=True):
-        return {}
     
-# A custom function to clean the text before sending it into the vectorizer
+# Utility function to clean text before sending it to some post-processing
 def cleanText(text):
     # get rid of newlines
     text = text.strip().replace("\n", " ").replace("\r", " ")
@@ -59,8 +27,7 @@ def cleanText(text):
 
     return text
 
-# A custom function to tokenize the text using spaCy
-# and convert to lemmas
+# Tokenize the text using spaCy and convert to lemmas
 def tokenizeText(sample):
 
     # get the tokens using spaCy
@@ -89,44 +56,44 @@ def tokenizeText(sample):
         tokens.remove("\n\n")
 
     return tokens
-
-def printNMostInformative(vectorizer, clf, N):
-    """Prints features with the highest coefficient values, per class"""
-    feature_names = vectorizer.get_feature_names()
-    coefs_with_fns = sorted(zip(clf.coef_[0], feature_names))
-    topClass1 = coefs_with_fns[:N]
-    topClass2 = coefs_with_fns[:-(N + 1):-1]
-    print("Class 1 best: ")
-    for feat in topClass1:
-        print(feat)
-    print("Class 2 best: ")
-    for feat in topClass2:
-        print(feat)
         
-################################## MAIN AREA #################################
-import pandas as pd
-from random import randrange
+if __name__ == '__main__':
+    import pandas as pd
+    from random import randrange
+    
+    # Set up spaCy
+    from spacy.en import English
+    parser = English()
 
-pro_gun_text = pd.read_csv('data/pro_gun_text.csv', dtype='str', header=None)
-pro_gun_text_list = pro_gun_text.ix[:,1].tolist()
+    # A custom stoplist
+    STOPLIST = set(stopwords.words('english') + ["n't", "'s", "'m", "ca"] + list(ENGLISH_STOP_WORDS))
+    # List of symbols we don't care about
+    SYMBOLS = " ".join(string.punctuation).split(" ") + ["-----", "---", "...", "“", "”", "'ve"]
+    
+    # import selected list of tweets
+    pro_gun_text = pd.read_csv('data/pro_gun_text.csv', dtype='str', header=None)
+    pro_gun_text_list = pro_gun_text.ix[:,1].tolist()
+    
+    #print("DEBUG::the tweet list is:")
+    #print(pro_gun_text_list)
+    # print("DEBUG::the length of the tweet list is:")
+    # print(len(pro_gun_text_list))
+    
+    # extract named entities of a randomly drawn sample tweet
+    idx = randrange(len(pro_gun_text_list))
+    example = pro_gun_text_list[idx]
+    
+    print("DEBUG::randomly drawn example tweet:")
+    print(example)
+    
+    parsedEx = parser(example)
+    
+    # use the below code to list all tokens
+    #for token in parsedEx:
+    #    print(token.orth_, token.ent_type_ if token.ent_type_ != "" else "(not an entity)")
 
-#print("DEBUG::the pro_gun_text is:")
-#print(pro_gun_text_list)
-print("DEBUG::the length of the resulting pro_gun list is:")
-print(len(pro_gun_text_list))
-
-# Let's look at the named entities of some tweets
-idx = randrange(len(pro_gun_text_list))
-example = pro_gun_text_list[idx]
-print("DEBUG::example sentence:")
-print(example)
-
-parsedEx = parser(example)
-for token in parsedEx:
-    print(token.orth_, token.ent_type_ if token.ent_type_ != "" else "(not an entity)")
-
-print("-------------- entities only ---------------")
-# if you just want the entities and nothing else, you can do access the parsed examples "ents" property like this:
-ents = list(parsedEx.ents)
-for entity in ents:
-    print(entity.label, entity.label_, ' '.join(t.orth_ for t in entity))
+    print("-------------- the entities are: ---------------")
+    # use the below code to list entities only...
+    ents = list(parsedEx.ents)
+    for entity in ents:
+        print(entity.label, entity.label_, ' '.join(t.orth_ for t in entity))
